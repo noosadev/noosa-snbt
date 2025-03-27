aeac7b6baf5020a488fa1998d1065a6a5a0df70b
kevinmantika <56221016+kevinmantika@users.noreply.github.com>
2025-03-27 10:10:23 +0700

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,17 +1,23 @@
 <?php
 
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-  /**
-   * Run the database seeds.
-   *
-   * @return void
-   */
-  public function run()
-  {
-    $this->call(UsersTableSeeder::class);
-    $this->call(SchoolsTableSeeder::class);
-  }
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+    }
 }
