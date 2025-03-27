ec8f93d0918c08abca0d5eb8088d00aaf9b526c7
kevinmantika <56221016+kevinmantika@users.noreply.github.com>
2025-03-27 09:04:25 +0700

    Update Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+<<<<<<< HEAD
 abstract class Controller
 {
     //
+=======
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+>>>>>>> e480621 (Update Controller)
 }
